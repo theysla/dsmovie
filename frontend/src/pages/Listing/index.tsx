249fc8c96d718ef4fdd1dd2d036087b830481089
Ysla Chaves <ysla.chaves.b@gmail.com>
2022-03-11 16:09:59 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,32 @@
+import MovieCard from "components/Navbar/MovieCard";
 import Pagination from "components/Navbar/Pagination";
 
 function Listing() {
 
     return (
-        <Pagination />
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6m col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6m col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                        <div className="col-sm-6m col-lg-4 col-xl-3 mb-3">
+                            <MovieCard />
+                        </div>
+                        <div className="col-sm-6m col-lg-4 col-xl-3 mb-3">
+                            <MovieCard />
+                        </div>
+                        <div className="col-sm-6m col-lg-4 col-xl-3 mb-3">
+                            <MovieCard />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
